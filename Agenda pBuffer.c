e6df98227e9bc139d4af267320e0378ee5295652
gustavoskop <gkopereck@gmail.com>
2025-06-03 22:34:26 -0300

    literalmente só o começo

---
+++ Agenda pBuffer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM_PESSOA (sizeof(char) * 10 + sizeof(int) + sizeof(char) * 10)
+
+    int main (){
+        void *pBuffer = malloc (sizeof(int) * 2); // menu + qtd de pessoas
+
+
+    }
