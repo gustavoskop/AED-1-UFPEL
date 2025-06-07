4fedaaa5037e0214d3a8eab0b819c392aa1743c1
gustavoskop <gkopereck@gmail.com>
2025-06-07 00:46:04 -0300

    quase acabando

--- AgendapBuffer.c
+++ AgendapBuffer.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 #define TAM_NOME sizeof(char) * 100
-#define TAM_IDADE sizeof(int) 
+#define TAM_IDADE sizeof(char) * 5 
 #define TAM_EMAIL sizeof(char) * 100
 #define TAM_PESSOA (TAM_NOME + TAM_IDADE + TAM_EMAIL)
-#define MENU sizeof(int)
+#define FIM (MENU + TAM_PESSOA)
+#define MENU sizeof(int) 
 
-void Adiciona(void *pBuffer, char* nome, char * idade, char* email, void * fim);
-void Busca();
+void Adiciona(void** pBuffer, int* menu, char* nome, char* idade, char* email, void **fim);
+bool Busca(void* pBuffer, char* nome, void** temp, void *fim);
 void Remove();
 void Lista();
+void ProxCampo(void** temp);
 
 
 int main()
@@ -27,21 +30,19 @@ int main()
     char *nome = NULL, *email = NULL, *idade = NULL;
     void *fim = NULL, *temp = NULL;
 
-
     menu = (int *) pBuffer;
     *menu = 0;
 
  
-
     nome = (pBuffer + MENU);
     idade = (pBuffer + MENU + TAM_NOME);
     email = (pBuffer + MENU + TAM_NOME + TAM_IDADE);
-    fim = (pBuffer + MENU + TAM_PESSOA);
+    fim  = (pBuffer + FIM);
 
-       printf("%lld", fim - pBuffer);
+       printf("%lld\n", (long long int)((char *)fim - (char *)pBuffer));
 
     while (*menu != 5){
-        printf("Escolha uma opcao: \n \
+        printf("\n\tEscolha uma opcao: \n \
         1. Adicionar uma pessoa \n \
         2. Remover uma pessoa \n \
         3. Buscar um nome \n \
@@ -52,7 +53,7 @@ int main()
 
         switch (*menu){
             case 1:
-            Adiciona(pBuffer, nome, idade, email, fim);
+            Adiciona(&pBuffer, menu, nome, idade, email, &fim);
             break;
 
             case 2:
@@ -60,11 +61,20 @@ int main()
             break;
 
             case 3:
-           // Busca();
+            printf("\nDigite o nome da pessoa: ");
+            fgets(nome, TAM_NOME, stdin);
+            nome [strcspn(nome, "\n")] = '\0';
+            if (Busca(pBuffer, nome, &temp, fim)){
+                printf("\n\tNome: %s", (char *)temp);
+                ProxCampo(&temp);
+                printf("\n\tIdade: %s", (char *)temp);
+                ProxCampo(&temp);
+                printf("\n\tEmail: %s", (char *)temp);
+            }
             break;
 
             case 4:
-            //Lista();
+            Lista(pBuffer, &temp, fim);
             break;
 
             case 5:
@@ -80,36 +90,86 @@ int main()
 
 }
 
-void Adiciona(void* pBuffer, char* nome, char* idade, char* email, void* fim){
-
+void Adiciona(void** pBuffer, int* menu, char* nome, char* idade, char* email, void **fim){
 
     printf("\n\tEscreva o nome: ");
     fgets(nome, TAM_NOME, stdin);
     nome [strcspn(nome, "\n")] = '\0';
 
-    printf("\n\tEscreva a idade: ");
+    printf("\tEscreva a idade: ");
     fgets(idade, TAM_IDADE, stdin);
     idade [strcspn(idade, "\n")] = '\0';
 
-    printf("\n\tEscreva o email: ");
+    printf("\tEscreva o email: ");
     fgets(email, TAM_EMAIL, stdin);
     email [strcspn(email, "\n")] = '\0';
 
 
 
-    pBuffer = realloc(pBuffer, (((long long int)(fim - pBuffer)) + (sizeof(char) * strlen(nome) + sizeof(int) + sizeof(char) * strlen(email))));
+    *pBuffer = realloc(*pBuffer, (((long long int)((char *)*fim - (char *)*pBuffer)) + \
+                                        (sizeof(char) * strlen(nome) + 1 + sizeof(char) * strlen(idade) + 1 + sizeof(char) * strlen(email) + 1)));
+    if(*pBuffer == NULL)
+        printf("nao foi possivel realocar memoria");
 
-    sscanf(nome, "%s", fim);
-    fim = fim + sizeof(char) * strlen(nome);
-    sscanf(idade, "%d", fim);
-    fim = fim + sizeof(int);
-    sscanf(email, "%s", fim);
-    fim = fim + sizeof(char) * strlen(email);
+    menu = (int *)*pBuffer;
+    nome = (*pBuffer + MENU);
+    idade = (*pBuffer + MENU + TAM_NOME);
+    email = (*pBuffer + MENU + TAM_NOME + TAM_IDADE);
 
-    fim = fim + strlen(nome) + sizeof(int) + strlen(email);
-
-
-
+    *fim = (char *)(*pBuffer) + ((char *)*fim - (char *)*pBuffer);
 
 
+    strcpy((char *)*fim, nome);
+    *fim = (char *)*fim + strlen(nome) + 1;
+    strcpy((char *)*fim, idade);
+    *fim = (char *)*fim + strlen(idade) + 1;
+    strcpy((char *)*fim, email);
+    *fim = (char *)*fim + strlen(email) + 1;
+    
+    printf("%lld\n", (long long int)((char *)*fim - (char*) *pBuffer));
+
+}
+
+void ProxCampo(void** temp){
+    *temp = (char *)*temp + strlen((char *)*temp) + 1;
+    }
+
+
+
+bool Busca(void* pBuffer, char* nome, void** temp, void *fim){
+    *temp = pBuffer + FIM;
+
+    if (*temp == NULL){
+        printf("\nAgenda vazia!");
+        return false;   
+    }
+    else {
+        while(*temp != fim){
+            if (strcmp(nome, (char *)*temp) == 0)
+                return true;
+            else {
+                ProxCampo(temp);
+                ProxCampo(temp);
+                ProxCampo(temp);
+        }
+        }
+        printf("\nNome nao encontrado!");
+        return false;
+    }
+    
+}
+
+void Lista(void* pBuffer, void** temp, void *fim){
+    *temp = pBuffer + FIM;
+
+        printf("\n--------------------------");
+    while (*temp != fim){
+        printf("\n\tNome: %s", (char *)*temp);
+        ProxCampo(temp);
+        printf("\n\tIdade: %s", (char *)*temp);
+        ProxCampo(temp);
+        printf("\n\tEmail: %s\n", (char *)*temp);
+        ProxCampo(temp);
+        printf("--------------------------");
+    }
 }
